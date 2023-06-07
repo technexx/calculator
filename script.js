5ad49670fc2fffd95885b87cc840dc3c55baafb0
Brian Salomone <brian.j.salomone@gmail.com>
2023-06-06 18:39:41 -0700

    consolidated button clicks

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const display = document.querySelector(".display")
 const leftButtonsContainer = document.querySelector(".number-buttons")
-const numberArray = [7, 8, 9, 4, 5, 6, 1, 2 ,3, 0, "."]
+const numberButtonArray = [7, 8, 9, 4, 5, 6, 1, 2 ,3, 0, "."]
 
 let displayValue = "0"
 display.innerText = displayValue
 
-for (let i=0; i<numberArray.length; i++) {
+let typeOfCalculation = ""
+let workingNumberArray = [0, 0]
+
+setManipulationButtonListeners()
+
+for (let i=0; i<numberButtonArray.length; i++) {
     createValueButtons(i)
 }
 
 function createValueButtons(position) {
     const content = document.createElement("button")
-    content.id = ("id", "button-" + numberArray[position])
-    content.textContent = numberArray[position]
+    content.id = ("id", "button-" + numberButtonArray[position])
+    content.textContent = numberButtonArray[position]
     content.style.height = "100px"
     content.style.width = "150px"
 
@@ -20,14 +25,55 @@ function createValueButtons(position) {
 
     leftButtonsContainer.appendChild(content)
 
-    setButtonListeners(content, position)
+    setNumberButtonListeners(content, position)
 }
 
-function setButtonListeners(element, position) {
+function setNumberButtonListeners(element, position) {
     element.addEventListener("click", () => {
-        displayValue += numberArray[position]
+        displayValue += numberButtonArray[position]
         display.innerText = displayValue
     })
 }
 
+function setManipulationButtonListeners() {
+    // let buttons = document.querySelector(".right-buttons").children
+    let buttons = document.querySelectorAll(".right-buttons button")
+    console.log(buttons)
 
+    for (let i=0; i<buttons.length; i++) {
+        buttons[i].addEventListener("click", () => {
+            console.log(buttons[i].id)
+
+            if (buttons[i].id === "divide-button") {
+                displayValue += " / "
+                typeOfCalculation = "division"
+            }
+
+            if (buttons[i].id === "multiply-button") {
+                displayValue += " * "
+                typeOfCalculation = "multiplication"
+            }
+
+            if (buttons[i].id === "subtract-button") {
+                displayValue += " - "
+                typeOfCalculation = "subtraction"
+            }
+            if (buttons[i].id === "add-button") {
+                displayValue += " + "
+                typeOfCalculation = "addition"
+            }
+            if (i.id === "equals-button") {
+            }
+
+            display.innerText = displayValue
+        })
+    }
+}
+
+function add(numOne, numTwo) { return numOne + numTwo };
+
+function subtract(numOne, numTwo) { return numOne - numTwo};
+
+function multiply(numOne, numTwo) { return numOne * numTwo };
+
+function divide(numOne, numTwo) { return numOne / numTwo };
